89edbb24f8bc66fb10d99439356abd4a3dfa02df
srikarshetty2511-oss <srikarshetty2511@gmail.com>
2026-03-10 08:01:06 +0000

    deleting django

--- backend/apps/chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ChatView,RAGChatView
-
-urlpatterns = [
-    path("", ChatView.as_view(), name="chat"),
-    path("rag/", RAGChatView.as_view(), name="rag-chat"),
-]
--- backend/apps/documents/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UploadPDFView
-
-urlpatterns = [
-    path("upload/", UploadPDFView.as_view()),
-]
